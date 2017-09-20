a78f73f2217d9ab1898a352ad7853c435031809b
an244 <thienan.le244@gmail.com>
2017-09-20 16:51:40 +0700

    cach thu 2 de hien thi data len web

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ app.set('view engine', 'ejs');
 app.set('views','./views');
 app.use(express.static('public'));
 
-let arrWords = [];
-app.get('/',(req,res)=> res.render('home', {arrWords}));
-
+let WordsCollection;
+app.get('/',(req,res)=> {
+    WordsCollection.find().toArray() //tra ve la 1 cai promise
+    .then(result => res.render('home',{arrWords: result}))
+    .catch(err => res.send(err.message));
+});
 
 const url = 'mongodb://localhost:27017/shop';
 
 MongoClient.connect(url)
 .then(db => {
     app.listen(3000, ()=> console.log('server started'));
-    const words = db.collection('words');//lay ra collection
-    return words.find().toArray();
+    WordsCollection = db.collection('words');//lay ra collection    
 })
-.then(result => arrWords = result)
+
 .catch(err=> console.log(err.message));
 
+//https://www.youtube.com/watch?v=wQkXLWME88s&feature=youtu.be
