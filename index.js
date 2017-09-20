59d0bdc527751790cb5c4660c1946e8e1e26faf6
an244 <thienan.le244@gmail.com>
2017-09-20 18:12:30 +0700

    add 1 tu vao WordsCollection

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ const MongoClient = require ('mongodb').MongoClient
 
 const express = require('express');
 const app = express();
+const parser = require('body-parser').urlencoded({extended:false});
+
 app.set('view engine', 'ejs');
 app.set('views','./views');
 app.use(express.static('public'));
 
 let WordsCollection;
 app.get('/',(req,res)=> {
-    WordsCollection.find().toArray() //tra ve la 1 cai promise
+    WordsCollection.find().toArray() //Object WordsCollection de tra ve method find() tra ve la 1 cai promise
     .then(result => res.render('home',{arrWords: result}))
     .catch(err => res.send(err.message));
+});
+
+app.post('/add', parser, (req,res)=> {
+     const {en, vn} = req.body;//lay tham so en, vn da duoc dat ten ben home.ejs
+     WordsCollection.insert({en, vn})
+     .then(()=> res.redirect('/'))
+     .catch(er => res.send(er.message));
 });
 
 const url = 'mongodb://localhost:27017/shop';
