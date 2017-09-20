9e75d2e09da975e560b09be5a0494603687e74e4
an244 <thienan.le244@gmail.com>
2017-09-20 15:35:34 +0700

    them home.ejs

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const MongoClient = require ('mongodb').MongoClient
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
-app.set('views','/views');
+app.set('views','./views');
 app.use(express.static('public'));
 
 app.get('/',(req,res)=> res.render('home'));
