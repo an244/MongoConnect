7e3f3e06c8663ba389411e1c97bb5ee76a97e303
NHA <NHA@LTQ-2.local>
2017-09-20 15:12:38 +0700

    setup server express()

--- a.js
+++ a.js
@@ -1,12 +1,22 @@
 const MongoClient = require ('mongodb').MongoClient
 //viet gon lai: const {MongoClient} = require ('mongod')
 
-const url = 'mongodb://localhost:27017/Users/NHA/Desktop/MongoData/shop';
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views','/views');
+app.use(express.static('public'));
+
+app.get('/',(req,res)=> res.render('home'));
+
+const url = 'mongodb://localhost:27017/shop';
 
 MongoClient.connect(url)
 .then(db => {
-    const words = db.collection('word');
+    app.listen(3000, ()=> console.log('server started'));
+    const words = db.collection('words');
     return words.find().toArray()//dung return de lay ket qua array tra ve
 })
 .then(result => console.log(result))
 .catch(err=> console.log(err.message));
+
