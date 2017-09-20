b0900c8facce4db996110c1977b7d1ba51330213
an244 <thienan.le244@gmail.com>
2017-09-20 16:22:47 +0700

    cach thu 1 de hien data ra web

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ app.set('view engine', 'ejs');
 app.set('views','./views');
 app.use(express.static('public'));
 
-app.get('/',(req,res)=> res.render('home'));
+let arrWords = [];
+app.get('/',(req,res)=> res.render('home', {arrWords}));
+
 
 const url = 'mongodb://localhost:27017/shop';
 
 MongoClient.connect(url)
 .then(db => {
     app.listen(3000, ()=> console.log('server started'));
-    const words = db.collection('words');
-    return words.find().toArray()//dung return de lay ket qua array tra ve
+    const words = db.collection('words');//lay ra collection
+    return words.find().toArray();
 })
-.then(result => console.log(result))
+.then(result => arrWords = result)
 .catch(err=> console.log(err.message));
 
